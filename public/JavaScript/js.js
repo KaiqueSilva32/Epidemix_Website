5f594b46df5df1082859bdd23dab225371f6c445
KaiqueSilva32 <silvakaique7934@gmail.com>
2024-11-13 16:01:48 -0300

    Inserção da sidebar feita, e nova tela de login

--- public/JavaScript/js.js
+++ public/JavaScript/js.js
@@ -1,21 +1,6 @@
+const sidebar = document.querySelector(".sidebar")
+const sidebarToggler = document.querySelector(".sidebar-toggler");
 
-
-var menuItem = document.querySelectorAll('.item-menu')
-
-function selectLink () {
-    menuItem.forEach((item) => 
-        item.classList.remove('ativo')
-    )
-    this.classList.add('ativo')
-}
-
-menuItem.forEach ((item) => 
-    item.addEventListener('click', selectLink)
-)
-// Expandir o menu
-var btnExp = document.querySelector('#btn-exp')
-var menuSide = document.querySelector('.menu-lateral')
-
-btnExp.addEventListener('click', function() {
-    menuSide.classList.toggle('expandir')
+sidebarToggler.addEventListener("click", () => {
+    sidebar.classList.toggle("collapsed")
 })
